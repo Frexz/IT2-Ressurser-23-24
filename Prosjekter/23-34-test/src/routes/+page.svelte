dba3fd3eb38515e9cb5cbaa19e49d55add332e94
Frexz <fredrik.pettersen3@gmail.com>
2023-08-22 09:59:56 +0200

    la til arbeidsprosjekt med variabler

---
+++ Prosjekter/23-34-test/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    let fagnavn = "Fysikk 1"
+</script>
+
+<h1>Velkommen til {fagnavn}</h1>
+<p>Jeg gleder meg veldig.</p>
+
+<style>
+    p {
+        color: blue;
+        font-size: 20px;
+        font-family: 'Courier New', Courier, monospace;
+    }
+
+    h1 {
+        color: rebeccapurple;
+    }
+</style>
---
+++ Prosjekter/23-34-variabler/src/routes/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    // Opprett variablene 'koppA' og 'koppB', og gi dem henholdsvis verdiene 'kaffe' og 'te'
+
+    function bytt() {
+        // Skriv en funksjon som bytter innholdet i koppA og koppB
+    }
+</script>
+
+<main>
+    <div class="kaffe">
+        <img src="src/lib/{koppA}.jpg" alt="Kaffe">
+    </div>
+    
+    <button on:click={bytt}>Bytt</button>
+    
+    <div class="te">
+        <img src="src/lib/{koppB}.jpg" alt="Te">
+    </div>
+</main>
+
+<style>
+    img {
+        height: 200px;
+        width: 200px;
+    }
+
+    main {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-top: 30vh;
+    }
+
+    .kaffe, .te {
+        height: 200px;
+        margin: 0px 40px;
+        width: 200px;
+    }
+</style>
