c8ef35d92cf1bbfc77f878dbad85d6286db41300
Frexz <fredrik.pettersen3@gmail.com>
2023-08-31 20:16:44 +0200

    la til 1.28 og 1.29

--- Prosjekter/23-Kapittel1-oppgaver/src/routes/1.4/oppg/1.28/+page.svelte
+++ Prosjekter/23-Kapittel1-oppgaver/src/routes/1.4/oppg/1.28/+page.svelte
@@ -1,9 +1,57 @@
 <script>
-    
+    let navn
+    let alder
+    let hundenavn
+    let hundealder
 </script>
+
+<div class="innhold">
+    <div class="inputfelt">
+        <input type="text" placeholder="Skriv inn ditt navn" bind:value={navn}>
+        <input type="number" placeholder="Skriv inn din alder" bind:value={alder}>
+        <input type="text" placeholder="Skriv inn hunden din sitt navn" bind:value={hundenavn}>
+        <input type="number" placeholder="Skriv inn hunden din sin alder" bind:value={hundealder}>
+    </div>
+
+    <div class="tekst">
+        <h1>Hei, {navn}!</h1>
+        <h1>Du er {alder} år gammel.</h1>
+
+        <h2>Hunden din heter {hundenavn}.</h2>
+        <h2>Den er {hundealder} år.</h2>
+
+        <h3>Alderen din tilsvarer {alder*5} hundeår</h3>
+    </div>
+</div>
+
 
 
 
 <style>
-    
+    h1 {
+        color: green;
+    }
+
+    h2 {
+        color: pink;
+    }
+
+    h3 {
+        color: purple;
+    }
+
+    input {
+        margin: 10px;
+    }
+
+    .innhold {
+        display: flex;
+    }
+
+    .inputfelt {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-right: 50px;
+    }
 </style>
--- Prosjekter/23-Kapittel1-oppgaver/src/routes/1.4/oppg/1.29/+page.svelte
+++ Prosjekter/23-Kapittel1-oppgaver/src/routes/1.4/oppg/1.29/+page.svelte
@@ -2,8 +2,34 @@
     
 </script>
 
+<ul>
+    <li>Man kan skjøte sammen strenger med konkatenering. Konkatenering innebærer bare at man bruker +-operasjonen
+        på strenger. For eksempel så vil strengen <i>"grise"</i> og strengen <i>"flaks"</i> bli <i>"{"grise" + "flaks"}"</i>
+        under operasjonen <i>"grise" + "flaks"</i>.
+    </li>
 
+    <li>
+        Hvis man har en streng med verdien <i>"23"</i> kan man konvertere denne verdien til typen number med funksjonen <i>Number()</i>
+        slik: <i>Number("23")</i>.
+    </li>
+
+    <li>
+        Hvis man har et tall med verdien <i>23</i> kan man kovertere denne verdien til typen string med funksjonen <i>String()</i>
+        slik: <i>String(23)</i>
+    </li>
+
+    <li>
+        For å sjekke hvilken type en verdi har kan man bruke funksjonen <i>typeof</i> slik at <i>typeof "23"</i> er {typeof "23"}
+        og <i>typeof 23</i> er {typeof 23}.
+    </li>
+</ul>
 
 <style>
-    
+    ul {
+        list-style: lower-alpha;
+    }
+
+    li {
+        margin: 20px;
+    }
 </style>
