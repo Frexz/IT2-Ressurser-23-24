9cf210b675ef482df2d646a620c160d6d4dac1c0
Frexz <fredrik.pettersen3@gmail.com>
2023-08-31 09:46:07 +0200

    la til diverse oppgaver

--- Prosjekter/23-Kapittel1-oppgaver/src/routes/1.3/oppg/1.21/+page.svelte
+++ Prosjekter/23-Kapittel1-oppgaver/src/routes/1.3/oppg/1.21/+page.svelte
@@ -1,9 +1,32 @@
 <script>
-    
+    let x1 = 7
+    let a = 3
+    let b = 4
+    let t = 4.09
+    let x2 = 3
 </script>
 
+<ul>
+    <li>
+        <h3>2<sup>x</sup> = {2**x1} når x = {x1}</h3>
+    </li>
+
+    <li>
+        <h3>&#8730;a<sup>2</sup> + b<sup>2</sup> = {Math.sqrt(a**2 + b**2)} der a = {a} og b = {b}</h3>
+    </li>
+
+    <li>
+        <h3>e<sup>0.28t</sup> = {Math.E**(0.28*t)} der t = {t}</h3>
+    </li>
+
+    <li>
+        <h3>10ln(x<sup>9</sup>) + 1.12 = {10*Math.log(x2**9) + 1.12} der x = {x2}</h3>
+    </li>
+</ul>
 
 
 <style>
-    
+    ul {
+        list-style: lower-alpha;
+    }
 </style>
--- Prosjekter/23-Kapittel1-oppgaver/src/routes/1.3/oppg/1.22/+page.svelte
+++ Prosjekter/23-Kapittel1-oppgaver/src/routes/1.3/oppg/1.22/+page.svelte
@@ -1,9 +1,17 @@
 <script>
-    
+    let tilfeldigTall = 10*Math.random() + 10  
+    let tilfeldigHeltall = Math.floor(1000*Math.random())
+    let tilfeldigHalvtall = 10*random()
 </script>
 
-
+<div class="innhold">
+    <p><b>Tilfeldig tall mellom 10 og 20:</b> {tilfeldigTall}</p>
+    <p><b>Tilfeldig heltall mellom 0 og 1000:</b> {tilfeldigHeltall}</p>
+</div>
 
 <style>
-    
+    .innhold {
+        display: flex;
+        flex-direction: column;
+    }
 </style>
