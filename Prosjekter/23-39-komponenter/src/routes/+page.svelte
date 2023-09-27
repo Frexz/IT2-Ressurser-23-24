7db86766692a2bc81ffe613b18110d28401d04f6
Frexz <fredrik.pettersen3@gmail.com>
2023-09-27 09:23:17 +0200

    Oppdatert arbeidsprosjekt om komponenter

---
+++ Prosjekter/23-39-komponenter/src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Terning from "./_komponenter/Terning.svelte";  
+
+    let liste = [1, 2, 3, 4, 5]
+    let kastAlle = false
+</script>
+
+<div class="innhold">
+    <div class="terninger">
+        {#each liste as a}
+            <Terning bind:kastAlle />
+        {/each}
+    </div>
+    <button on:click={() => kastAlle = true}>Kast alle terningene</button>
+</div>
+
+<style>
+    button {
+        height: 50px;
+        margin-top: 30px;
+        width: 650px;
+    }
+
+    .terninger {
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+    }
+
+    .innhold {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        width: 100vw;
+    }
+</style>
---
+++ Prosjekter/23-39-komponenter/src/routes/_komponenter/Terning.svelte
@@ -0,0 +1,40 @@
+<script>
+    import { kastTerning } from "../../../../gjenbruk/myFunctions"
+
+    let tall = kastTerning()
+    export let kastAlle
+
+    $: {
+        if (kastAlle) {
+            tall = kastTerning()
+            kastAlle = false
+        }
+        
+    }
+</script>
+
+<div class="terning">
+    <img src="/terning/{tall}.png" alt="">
+    <button on:click={() => tall = kastTerning()}>Kast terning</button>
+</div>
+
+<style>
+    button {
+        border-radius: 50px;
+        color: white;
+        background-color: rgb(7, 7, 43);
+        padding: 5px;
+        width: 120px;
+    }
+
+    img {
+        margin-bottom: 10px;
+        width: 100px;
+    }
+    .terning {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
