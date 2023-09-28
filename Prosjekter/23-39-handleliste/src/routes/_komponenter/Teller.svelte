fabe59aabff364ae5a07ec2ef11b4515fd209279
Frexz <fredrik.pettersen3@gmail.com>
2023-09-28 11:11:32 +0200

    la til handleliste prosjekt

---
+++ Prosjekter/23-39-handleliste/src/routes/_komponenter/Teller.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let tall = 0
+
+    $: {
+        if (tall < 0) {
+            tall = 0
+        }
+    }
+</script>
+
+<div class="teller">
+    <button on:click={() => tall--}>-</button>
+    <p>{tall}</p>
+    <button on:click={() => tall++}>+</button>
+</div>
+
+<style>
+    button {
+        height: 20px;
+    }
+    .teller {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        gap: 10px;
+    }
+</style>
