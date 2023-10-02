e70905364fb6aa7bed98c5c054894b7f191ade75
Frexz <fredrik.pettersen3@gmail.com>
2023-10-02 11:25:20 +0200

    Ferdiggjort oppgave 2.15

--- Prosjekter/loesninger/src/routes/kapittel2/2.1/oppg/2.15/+page.svelte
+++ Prosjekter/loesninger/src/routes/kapittel2/2.1/oppg/2.15/+page.svelte
@@ -4,29 +4,54 @@
     let fraValuta
     let tilValuta
     let beløp
+    let veksletBeløp
+    let melding = ""
     let overskrifter = ["Beløp", "Fra Valuta", "Til Valuta"]
+
+    function håndterKlikk() {
+        veksletBeløp = veksleFraTil(beløp, fraValuta, tilValuta)
+        melding = `${beløp} ${fraValuta} tilsvarer ${veksletBeløp.toFixed(2)} ${tilValuta}`
+    }
 </script>
 
 <div class="brukerinputs">
     {#each overskrifter as overskrift}
         <p>{overskrift}</p>
     {/each}
-    <div class="beløp">
-        <input type="number" bind:value={beløp}>
-        <p>kr</p>
-    </div>
+
+    <input type="number" bind:value={beløp}>
+   
+    <select bind:value={fraValuta}>
+        <option value="">---</option>
+        <option value="EUR">Euro (EUR)</option>
+        <option value="USD">Amerikanske dollar (USD)</option>
+        <option value="GBP">Britiske pund (GBP)</option>
+        <option value="JPY">Japanske yen (JPY)</option>
+        <option value="NOK">Norske kroner (NOK)</option>
+    </select>
+
+    <select bind:value={tilValuta}>
+        <option value="">---</option>
+        <option value="EUR">Euro (EUR)</option>
+        <option value="USD">Amerikanske dollar (USD)</option>
+        <option value="GBP">Britiske pund (GBP)</option>
+        <option value="JPY">Japanske yen (JPY)</option>
+        <option value="NOK">Norske kroner (NOK)</option>
+    </select>
+
+    <button on:click={håndterKlikk}>Veksle</button>
+    <p>{melding}</p>
 </div>
 
 <style>
-    .beløp {
-        display: flex;
-        flex-direction: row;
-        gap: 5px;
+    input, select {
+        height: 20px;
     }
 
     .brukerinputs {
         display: grid;
-        grid-template-columns: repeat(3, 1fr);
+        align-items: center;
+        grid-template-columns: repeat(3, 300px);
         gap: 20px;
     }
 </style>
