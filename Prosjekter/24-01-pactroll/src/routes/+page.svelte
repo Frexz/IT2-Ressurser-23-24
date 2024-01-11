719ca91c9c131039b5e94572179b2a6932233436
Frexz <fredrik.pettersen3@gmail.com>
2024-01-11 09:53:53 +0100

    pactroll

---
+++ Prosjekter/24-01-pactroll/src/routes/+page.svelte
@@ -0,0 +1,149 @@
+<script>
+    import { onMount } from 'svelte'
+
+    let RUTELENGDE = 25
+    let KOLONNER = 40
+    let RADER = 20
+    let BREDDE = RUTELENGDE * KOLONNER
+    let HOYDE = RUTELENGDE * RADER
+    
+
+    class Troll {
+        constructor() {
+            this.x = RUTELENGDE * 19
+            this.y = RUTELENGDE * 10
+            this.vx = 0
+            this.vy = 0
+            this.farge = "lime"
+        }
+
+        move() {
+            this.x += this.vx * RUTELENGDE
+            this.y += this.vy * RUTELENGDE
+        }
+
+    }
+
+    class Mat {
+        constructor() {
+            this.x = RUTELENGDE * Math.floor(KOLONNER*Math.random())
+            this.y = RUTELENGDE * Math.floor(RADER*Math.random())
+            this.farge = "yellow"
+        }
+    }
+
+    class Hindring {
+        constructor(x, y) {
+            this.x = x
+            this.y = y
+            this.farge = "grey"
+        }
+    }
+
+    let canvas
+    let ctx
+    let id
+
+    let poeng = 0
+    let gameOver = false
+    let troll = new Troll()
+    let elementer = [troll]
+
+    for (let i = 0; i < 3; i++) {
+        elementer.push(new Mat())
+    }
+
+    
+    onMount(() => {
+        ctx = canvas.getContext("2d")
+        id = setInterval(() => update(ctx), 150)
+    })
+
+    function update(ctx) {
+        console.log("Ny Runde")
+        console.log(elementer)
+        ctx.fillStyle = "black"
+        ctx.fillRect(0, 0, BREDDE, HOYDE)
+
+        for (const element of elementer) {
+            ctx.fillStyle = element.farge
+            ctx.fillRect(element.x, element.y, RUTELENGDE, RUTELENGDE)
+        }
+
+        ctx.fillStyle = "coral"
+        ctx.font = "20px Times New Roman"
+        ctx.fillText(`Poeng: ${poeng}`, 20, 20)
+
+        troll.move()
+        sjekkKollisjon()
+        sjekkVegg()
+
+        if (gameOver) {
+            ctx.fillStyle = "red"
+            ctx.font = "40px Comic Sans"
+            ctx.fillText("GAME OVER!", BREDDE/2 - RUTELENGDE*5, HOYDE/2)
+        }
+    }
+
+    function handleKeydown(e) {
+        if (e.key == "w") {
+            troll.vx = 0
+            troll.vy = -1
+        } else if ( e.key == "s") {
+            troll.vx = 0
+            troll.vy = 1
+        } else if (e.key == "a") {
+            troll.vx = -1
+            troll.vy = 0
+        } else if (e.key == "d") {
+            troll.vx = 1
+            troll.vy = 0
+        }
+    }
+
+    function sjekkVegg() {
+        if (troll.y < 0 || troll.x >= BREDDE || troll.y >= HOYDE || troll.x < 0) {
+            gameOver = true
+            clearInterval(id)
+        }
+    }
+
+    function sjekkKollisjon() {
+        for (let i = 1; i < elementer.length; i++) {
+            if (elementer[i] instanceof Mat && troll.x == elementer[i].x && troll.y == elementer[i].y) {
+                spisMat(elementer[i])
+                return
+            } else if (elementer[i] instanceof Hindring && troll.x == elementer[i].x && troll.y == elementer[i].y) {
+                clearInterval(id)
+                gameOver = true
+                return
+            }
+        }
+    }
+
+    function spisMat(matbit) {
+        poeng++
+
+        elementer = elementer.filter((mat) => mat != matbit)
+        elementer.push(new Hindring(matbit.x, matbit.y))
+        elementer.push(new Mat())
+    }
+
+
+</script>
+
+<div class="innhold">
+    <canvas bind:this={canvas} height={HOYDE} width={BREDDE} />
+</div>
+
+<svelte:window on:keydown|preventDefault={(e) => handleKeydown(e)}/>
+
+<style>
+    .innhold {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        width: 100vw;
+    }
+</style>
