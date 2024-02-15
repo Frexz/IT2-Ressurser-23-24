c00dc1309ec25846e09641204becc3ca630307fb
Frexz <fredrik.pettersen3@gmail.com>
2024-02-15 10:39:56 +0100

    land.js

---
+++ Prosjekter/gjenbruk/klasser/land.js
@@ -0,0 +1,12 @@
+let land = [
+    {"name": "Brazil", "population": 212000000},
+    {"name": "Mexico", "population": 129000000},
+    {"name": "Vietnam", "population": 96480000},
+    {"name": "Germany", "population": 83783942},
+    {"name": "Bangladesh", "population": 163046161},
+    {"name": "Nigeria", "population": 206139589},
+    {"name": "Russia", "population": 145934462},
+    {"name": "Egypt", "population": 102334404},
+    {"name": "Turkey", "population": 84339067},
+    {"name": "Japan", "population": 126476461},
+]
